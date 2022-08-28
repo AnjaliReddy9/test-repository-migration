b09d9de1925c3443474179a7e295e6abd09bc1bc
Anjali Ega <anjaliega9@gmail.com>
2022-08-29 00:46:27 +0530

    Update world

--- src/demo/git.java
+++ src/demo/git.java
@@ -5,6 +5,6 @@ public class git {
 	public static void main(String[] args) {
 		System.out.println("Hii");
 		System.out.println("Hello");
-		System.out.println("World");
+		System.out.println("world");
 	}
 }
